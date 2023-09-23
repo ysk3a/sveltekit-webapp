35e6a6aab0239d8b6f14d8444dc3264df3b338b2
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-09-22 21:34:58 -0400

    virtual scroll, split pane

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,21 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let flavors: Record<string, boolean> = {
+		'virutal scroll': true,
+		aggrid: false,
+		split: true,
+		editor: false,
+		svelvet: false,
+		'ejs inf': false
+	};
+	function filter(flavor: string): void {
+		flavors[flavor] = !flavors[flavor];
+	}
+	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
 </script>
 
 <!-- App Shell -->
@@ -17,33 +31,58 @@
 				<strong class="text-xl uppercase">Skeleton</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				{#each Object.keys(flavors) as f}
+					<!-- svelte-ignore a11y-no-static-element-interactions -->
+					<span
+						class="chip {flavors[f] ? 'variant-filled' : 'variant-soft'}"
+						on:click={() => {
+							filter(f);
+						}}
+						on:keypress
+					>
+						<span class="capitalize">{f}</span>
+					</span>
+				{/each}
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Hidden below Tailwind's large breakpoint -->
+		<div id="sidebar-left" class="hidden lg:block">
+			<nav class="list-nav">
+				<!-- (optionally you can provide a label here) -->
+				<ul>
+					<li>
+						<a href="/">
+							<span class="badge bg-primary-500">💀</span>
+							<span class="flex-auto">Home</span>
+						</a>
+					</li>
+					<li>
+						<a href="/feed">
+							<span class="badge bg-primary-500">💀</span>
+							<span class="flex-auto">Feed</span>
+						</a>
+					</li>
+					<li>
+						<a href="/chat">
+							<span class="badge bg-primary-500">💀</span>
+							<span class="flex-auto">Chat</span>
+						</a>
+					</li>
+					<!-- ... -->
+				</ul>
+			</nav>
+		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">
+		<!-- Hidden below Tailwind's large breakpoint -->
+		<div id="sidebar-right" class="hidden lg:block">Right Sidebar</div>
+	</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
 </AppShell>
---
+++ src/routes/feed/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { afterUpdate, tick } from 'svelte';
+	import VirtualScroll from 'svelte-virtual-scroll-list';
+	import { createSequenceGenerator, randomInteger } from '$lib/mock';
+	import TestItem from '$lib/components/TestItem.svelte';
+
+	const getItemId = createSequenceGenerator();
+
+	let items = [];
+	addItems(true, 100);
+
+	let list: VirtualScroll;
+
+	function addItems(top = true, count = 10) {
+		let new_items = [];
+		for (let i = 0; i < count; i++)
+			new_items.push({ uniqueKey: getItemId(), height: randomInteger(20, 660) });
+		if (top) items = [...new_items, ...items];
+		else items = [...items, ...new_items];
+	}
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// console.log(':: feed/+page.svelte list=', list);
+	});
+	afterUpdate(() => {
+		console.log('::afterUpdate');
+		if (list) {
+			list.scrollToBottom();
+		}
+	});
+</script>
+
+<div class="vs">
+	<VirtualScroll bind:this={list} data={items} key="uniqueKey" let:data>
+		<div slot="header">This is a header</div>
+		<TestItem {...data} />
+		<div slot="footer">This is a footer</div>
+	</VirtualScroll>
+</div>
+<button class="btn variant-filled" on:click={addItems}>Add 10 to top</button>
+<button class="btn variant-filled" on:click={() => addItems(false)}>Add 10 to bottom</button>
+<button class="btn variant-filled" on:click={list.scrollToBottom}>To bottom</button>
+<button
+	class="btn variant-filled"
+	on:click={async () => {
+		addItems(false, 1);
+		await tick();
+		list.scrollToBottom();
+	}}
+	>Add 1 and scroll to bottom
+</button>
+<button class="btn variant-filled" on:click={() => (items[15].height = randomInteger(10, 150))}
+	>Random height for 15 item</button
+>
+
+<style>
+	.vs {
+		height: calc(100vh - 50px);
+	}
+</style>
